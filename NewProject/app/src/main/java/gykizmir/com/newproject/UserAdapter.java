074f1f3f6dc83d978e2dd04ded95ac89b15224f6
geallen <gamzesen3827@gmail.com>
2019-01-20 01:20:25 +0300

    Database value set from input text

--- NewProject/app/src/main/java/gykizmir/com/newproject/UserAdapter.java
+++ NewProject/app/src/main/java/gykizmir/com/newproject/UserAdapter.java
@@ -1,14 +1,26 @@
 package gykizmir.com.newproject;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -49,6 +61,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
         public TextView isim;
         public ImageView resim;
+        private DatabaseReference dbRef;
 
         public ViewHolder(View view){
             super(view);
@@ -60,8 +73,60 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
                 public void onClick(View v) {
                     int position = getAdapterPosition();
                     if(position != RecyclerView.NO_POSITION){
-                        User selectedUser = userList.get(position);
+                        final User selectedUser = userList.get(position);
                         Toast.makeText(context, "Secilen kullanici: "+ selectedUser.getName(), Toast.LENGTH_LONG).show();
+
+                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+                        alertDialog.setTitle("Degistir");
+                        alertDialog.setMessage("Yeni degeri girin");
+                        final EditText input = new EditText(context);
+                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.MATCH_PARENT);
+                        input.setLayoutParams(lp);
+                        alertDialog.setView(input);
+
+                        alertDialog.setPositiveButton("Onayla",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+
+// value su o toast icinde gosterilen deger olanin key i ni bul. onun valuesunu girilen degeri setle
+
+                                        dbRef = FirebaseDatabase.getInstance().getReference("users");
+
+                                        dbRef.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                                                    User u = dataSnapshot1.getValue(User.class);
+                                                    if(u.getName().equals(selectedUser.getName())){
+                                                             dbRef.child(dataSnapshot1.getKey()).child("name").setValue(input.getText().toString());
+
+                                                    }
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(DatabaseError databaseError) {
+
+                                            }
+                                        });
+
+                                    }
+                                });
+
+                        alertDialog.setNegativeButton("Iptal",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                        alertDialog.show();
+
+                        FirebaseDatabase.getInstance().getReference("users").child("gamze").child("name").setValue("Gamzecik");
+
                     }
                 }
             });
